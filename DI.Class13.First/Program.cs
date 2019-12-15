b5610da22fa15ee5cb40153b9a1e96dfc04c8dae
aamir <wasi3aamir@gmail.com>
2019-12-15 13:16:48 +0600

    DI.Class13.First

---
+++ DI.Class13.First/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DI.Class13.First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
