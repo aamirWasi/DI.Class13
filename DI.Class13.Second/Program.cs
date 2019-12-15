2bb807b9f8960ae6bed28bd653ccc5257a27c83e
aamir <wasi3aamir@gmail.com>
2019-12-15 14:44:23 +0600

    With RegisterAssemblyTypes and DI

--- DI.Class13.Second/Program.cs
+++ DI.Class13.Second/Program.cs
@@ -1,5 +1,6 @@
-﻿using DI.Class13.Second.Repository.Concrete;
+﻿using Autofac;
 using System;
+using System.Reflection;
 
 namespace DI.Class13.Second
 {
@@ -7,11 +8,25 @@ namespace DI.Class13.Second
     {
         static void Main(string[] args)
         {
-            var printer = new Printer();
-            var numericStore = new InMemoryNumericStore();
-            var optionService = new OptionService(numericStore, printer);
-            var application = new Application(optionService, printer);
-            application.Run();
+            //var printer = new Printer();
+            //var numericStore = new InMemoryNumericStore();
+            //var optionService = new OptionService(numericStore, printer);
+            //var application = new Application(optionService, printer);
+            //application.Run();
+
+            //with Autofac and DI
+            var container = BuildContainer();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                scope.Resolve<Application>().Run();
+            }
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsSelf().AsImplementedInterfaces();
+            return builder.Build();
         }
     }
 }
